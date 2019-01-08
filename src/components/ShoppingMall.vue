f39afc629d46d8c29cb3acb2c51e49b6d9bc9eec
410323083 <410323083@qq.com>
2019-01-08 15:34:32 +0800

    project init

---
+++ src/components/ShoppingMall.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="shopping-mall">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: ""
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='scss'>
+
+</style>
